4ebe31a3490d09d82feabd125491c45473bf882b
mashan <masakazusaito94@gmail.com>
2013-07-14 17:05:19 +0900

    Commandパターンの追加

---
+++ ruby/DesignPattern/Iterator/array_iterator.rb
@@ -0,0 +1,29 @@
+class ArrayIterator
+  def initialize(array)
+    @array = array
+    @index = 0
+  end
+
+  def has_next?
+    @index < @array.length
+  end
+
+  def item
+    @array[@index]
+  end
+
+  def next_item
+    value = @array[@index]
+    @index += 1
+    value
+  end
+end
+
+array = ['red', 'blue', 'green']
+i = ArrayIterator.new(array)
+
+while i.has_next? do
+  puts i.item
+  i.next_item
+end
+
