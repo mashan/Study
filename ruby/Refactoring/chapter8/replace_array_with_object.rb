e0321edee5f40b63fdd0a4a28aaa179e40cb630b
mashan <masakazusaito94@gmail.com>
2013-09-21 14:12:19 +0900

    オブジェクトで管理。属性の意図がはっきりした

--- ruby/Refactoring/chapter8/replace_array_with_object.rb
+++ ruby/Refactoring/chapter8/replace_array_with_object.rb
@@ -1,6 +1,14 @@
-row = []
-row[0] = "Liverpool"
-row[1] = "15"
+class Performance
+  attr_accessor :name
+  attr_writer :wins
 
-name = row[0]
-wins = row[1].to_i
+  def wins
+    @wins.to_i
+  end
+end
+
+row = Performance.new
+row.name = "Liverpool"
+row.wins = "15"
+
+p row
