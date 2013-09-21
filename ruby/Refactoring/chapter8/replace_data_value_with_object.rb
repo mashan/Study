a3e410103f437c7a3c54355b65006aad277de078
mashan <masakazusaito94@gmail.com>
2013-09-21 12:50:00 +0900

    customerを別クラス化

--- ruby/Refactoring/chapter8/replace_data_value_with_object.rb
+++ ruby/Refactoring/chapter8/replace_data_value_with_object.rb
@@ -1,12 +1,26 @@
 class Order
-  attr_accessor :customer
+  def initialize(customer_name)
+    @customer = Customer.new(customer_name)
+  end
 
-  def initialize(customer)
-    @customer = customer
+  def customer_name
+    @customer.name
+  end
+
+  def customer=(customer_name)
+    @customer = Customer.new(customer_name)
   end
 
   private
   def self.number_of_orders_for(orders, customer)
     orders.select { |order| order.customer == customer }.size
+  end
+end
+
+class Customer
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
   end
 end
